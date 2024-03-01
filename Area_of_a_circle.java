78526bc9176f65676e5686fbb3773d3b473263c4
Ramya Dulam <155840420+Ramyadulam@users.noreply.github.com>
2024-03-01 11:27:00 +0530

    import java.util.*; public class ramya{     public static void main(String[] args)     {         Scanner sc=new Scanner(System.in);         int a=sc.nextInt();         System.out.printf("%.2f",3.14*a*a);     } }

---
+++ Area_of_a_circle.java
@@ -0,0 +1,9 @@
+import java.util.*;
+public class ramya{
+    public static void main(String[] args)
+    {
+        Scanner sc=new Scanner(System.in);
+        int a=sc.nextInt();
+        System.out.printf("%.2f",3.14*a*a);
+    }
+}
