32460394a7f0213bf0f73fb012c8fcdd5d33aaad
Ramya Dulam <155840420+Ramyadulam@users.noreply.github.com>
2024-03-01 11:14:58 +0530

    import java.util.*; public class Ramya{     public static void main(String[] args)     {         Scanner sc=new Scanner(System.in);         int n=sc.nextInt();         System.out.printf("%.2f",2.54*n);     } }

---
+++ Inches_to_Centimeters.java
@@ -0,0 +1,9 @@
+import java.util.*;
+public class Ramya{
+    public static void main(String[] args)
+    {
+        Scanner sc=new Scanner(System.in);
+        int n=sc.nextInt();
+        System.out.printf("%.2f",2.54*n);
+    }
+}
