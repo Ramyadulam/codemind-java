cd394f774e4fc57ffec4191a5943588680da50ec
Ramya Dulam <155840420+Ramyadulam@users.noreply.github.com>
2024-03-01 11:02:06 +0530

    import java.util.*; public class Ramya{     public static void main(String[] args)     {         Scanner sc=new Scanner(System.in);         double a=sc.nextInt();         double b=sc.nextInt();         double res=((b-a)/a*100);         System.out.printf("%.2f",res);     } }

---
+++ Profit_Percentage.java
@@ -0,0 +1,11 @@
+import java.util.*;
+public class Ramya{
+    public static void main(String[] args)
+    {
+        Scanner sc=new Scanner(System.in);
+        double a=sc.nextInt();
+        double b=sc.nextInt();
+        double res=((b-a)/a*100);
+        System.out.printf("%.2f",res);
+    }
+}
