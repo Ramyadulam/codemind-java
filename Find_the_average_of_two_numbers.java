9525e9d3779ae4dc2ff7824136a3fcac2f45916e
Ramya Dulam <155840420+Ramyadulam@users.noreply.github.com>
2024-03-01 11:23:03 +0530

    import java.util.*; public class ramya{     public static void main(String[] args)     {         Scanner sc=new Scanner(System.in);         double a=sc.nextInt();         double b=sc.nextInt();         double res=(a+b)/2;         System.out.printf("%.4f",res);     } }

---
+++ Find_the_average_of_two_numbers.java
@@ -0,0 +1,11 @@
+import java.util.*;
+public class ramya{
+    public static void main(String[] args)
+    {
+        Scanner sc=new Scanner(System.in);
+        double a=sc.nextInt();
+        double b=sc.nextInt();
+        double res=(a+b)/2;
+        System.out.printf("%.4f",res);
+    }
+}
